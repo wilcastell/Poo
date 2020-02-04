e4dbf0149368a40606414b0814857e0fc98dec01
castellwil <systemprogrampro@gmail.com>
2020-02-04 11:23:35 -0500

    encapsulamiento Poo PHP - Duilioo Palacios

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 
 class Person
 {
-	var $firstName;
-	var $lastName;
+	protected $firstName;
+	protected $lastName;
+	protected $nickName;
+	protected $changeNickName = 0;
+
 
 	public function __construct($firstName, $lastName)
 	{
@@ -11,7 +14,36 @@ class Person
 		$this->lastName = $lastName;
 	}
 
-	public function fullName()
+	public function getFirstName()
+	{
+		return $this->firstName;
+	}
+
+	public function getLastName()
+	{
+		return $this->lastName;
+	}
+
+	public function setFirstName($firstName)
+	{
+		$this->firstName = $firstName;
+	}
+
+	public function setNickName($nickName)
+	{
+		if ($this->changeNickName >= 2) {
+			throw new Exception("no puedes cambiar el nickName más de dos veces");
+		}
+			$this->nickName = $nickName;
+			$this->changeNickName++;
+	}
+
+	public function getNickName()
+	{
+		return $this->nickName;
+	}
+
+	public function getFullName()
 	{
 		return $this->firstName. ' '. $this->lastName;
 	}
@@ -24,4 +56,11 @@ $person2 = new Person('Zack','Morris');
 $person3 = new Person('Cristian','Castro');
 
 
-echo "{$person1->fullName()} es amigo de {$person2->fullname()} y queman los discos de {$person3->fullname()}";
+echo "{$person1->getFullName()} es amigo de {$person2->getFullname()} y queman los discos de {$person3->getFullname()}";
+$person3->setFirstName("Manuel");
+echo '<br> '.$person3->getFullname();
+
+$person3->setNickName('Parásito');
+$person3->setNickName('Sapo');
+$person3->setNickName('nada');
+echo $person3->getNickName();
